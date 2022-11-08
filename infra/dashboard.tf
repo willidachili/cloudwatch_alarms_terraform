cc6d38246bb3d3b3923f068652ccda694a3a5bd6
glennbech <glenn.bech@gmail.com>
2022-11-08 01:03:42 +0100

    Major update

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -21,25 +21,6 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of accounts"
       }
-    },
- {
-      "type": "metric",
-      "x": 0,
-       "y": 1,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.student_name}",
-            "bank_sum.value"
-          ]
-        ],
-        "period": 300,
-        "stat": "Maximum",
-        "region": "eu-west-1",
-        "title": "Bank Balance (Should be 0)"
-      }
     }
   ]
 }
